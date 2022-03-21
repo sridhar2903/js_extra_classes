5d405e8cc22e14ca473b673aa229037323b959ae
sridhar2903 <saisridhard@gmail.com>
2022-03-21 14:52:17 +0530

    day3

---
+++ day3/script.js
@@ -0,0 +1,14 @@
+//CODE TO CHECK WHETHER THE ARRAY ELEMENT VALUES IN BOTH THE ARRAYS WHICH HAVE BEEN CREATED AS OBJECT PROPERITIES IN TWO SEPARATE OBJECTS RESPECTIVELY
+let obj=
+{
+    a:"sridhar",
+    b:[1,2,3]
+}
+
+let obj1=
+{
+    c:"dheeraj",
+    d:[1,2,3]
+}
+
+console.log( obj.b[0,1,2]==obj1.d[0,1,2]);
