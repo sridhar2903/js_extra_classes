da59a597ec2ad5c3024493cd3257257e33d49319
sridhar2903 <saisridhard@gmail.com>
2022-03-28 13:16:34 +0530

    changes

---
+++ day7/mapfilter.js
@@ -0,0 +1,36 @@
+//MAP ,REDUCE ,FILTER IN ARRAYS
+
+// let arr=[1,2,3,4];
+
+// let a=arr.map((x)=>x*2);
+// let b=arr.filter((x)=>x%2===0)
+// let c=arr.reduce((x,s)=>s=s+x,0)
+// let d=arr.reduce((x,p)=>p=p*x,1)
+// console.log(d);
+// console.log(c);
+// console.log(a);
+// console.log(b);
+
+
+
+//ARRAY OPERATIONS:
+
+
+// let array=[1,2,3];
+
+// let e=array.push(4)
+// console.log(array);
+// let f=array.pop()
+// console.log(array);
+// let g=array.shift()
+// console.log(array);
+
+// let h=array.unshift(5);
+// console.log(array);
+
+// let i=array.splice(0,2);
+// console.log(array);
+
+// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+// console.log(animals.slice(animals.length));
